3311a1eb66c347e2d8836a78aaa28f7610a7b2fb
Dmitry R <rodioshkin@gmail.com>
2022-01-08 20:13:54 +0300

    comment from python script

---
+++ Push.py
@@ -0,0 +1,33 @@
+
+from os import rename
+from git import Repo
+from git.exc import GitError
+
+PATH_OF_GIT_REPO = r'/Users/dmitrirodioskin/Documents/git_repositories/test_sa/'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'comment from python script'
+
+def git_push_gitlab():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(all=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        
+        origin = repo.remote(add='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')
+
+def git_push_github():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(all=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        
+        origin = repo.remote(name='origin_github')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')
+ 
+git_push_github()
+git_push_gitlab()
+
